fffce6454ea5b09bf528f585735bdc8971016838
pranjaligit <pranjalikulkarni13@gmail.com>
2021-09-16 18:42:12 +0530

    first commit for cab management

---
+++ src/main/java/com/cg/cabaapi/CbaapiApplication.java
@@ -0,0 +1,13 @@
+package com.cg.cabaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/cabaapi/CbaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.cabaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
